e371a8a753ba7b88f13d820b2c294295ae21d8de
KarolZarebski <karolkarolski25@gmail.com>
2020-11-13 18:48:37 +0100

    Initial commit

---
+++ TPM/TPM/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TPM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
