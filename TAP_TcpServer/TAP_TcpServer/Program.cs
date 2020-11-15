c1ef05cd7b30a03cfe315ba95b43bc1b1e2a343a
KarolZarebski <karolkarolski25@gmail.com>
2020-11-15 11:34:48 +0100

    Added exception handling during application startup

--- TAP_TcpServer/TAP_TcpServer/Program.cs
+++ TAP_TcpServer/TAP_TcpServer/Program.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using ServerLibrary;
 using ServerLibrary.Services;
+using System;
 using WeatherLibrary;
 using WeatherLibrary.Services;
 
@@ -14,16 +15,23 @@ namespace TAP_TcpServer
 
         static void Main(string[] args)
         {
-            _configuration = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json")
-                .Build();
+            try
+            {
+                _configuration = new ConfigurationBuilder()
+                    .AddJsonFile("appsettings.json")
+                    .Build();
 
-            var servicesCollection = new ServiceCollection();
-            ConfigureServices(servicesCollection);
+                var servicesCollection = new ServiceCollection();
+                ConfigureServices(servicesCollection);
 
-            _serviceProvider = servicesCollection.BuildServiceProvider();
+                _serviceProvider = servicesCollection.BuildServiceProvider();
 
-            _serviceProvider.GetRequiredService<IServerService>().Server().Wait();
+                _serviceProvider.GetRequiredService<IServerService>().Server().Wait();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception occured during application startup, {ex}");
+            }
         }
 
         private static void ConfigureServices(IServiceCollection servicesCollection)
