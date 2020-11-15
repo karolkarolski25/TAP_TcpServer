8c6484d9affa835380f52f795af631c440cf139b
patryk9922 <33067910+patryk9922@users.noreply.github.com>
2020-11-15 14:43:56 +0100

    Added LoginService

---
+++ TAP_TcpServer/LoginLibrary/Services/LoginService.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace LoginLibrary.Services
+{
+    class LoginService : ILoginService
+    {
+        private Aes aes = Aes.Create();
+        private byte[] key, iv;
+
+        public bool CheckData(string data)
+        {
+            try
+            {
+                FileStream fileStream = new FileStream("NotPasswords.bin", FileMode.OpenOrCreate);
+                CryptoStream cryptoStream = new CryptoStream(fileStream, aes.CreateDecryptor(key, iv), CryptoStreamMode.Read);
+                StreamReader streamReader = new StreamReader(cryptoStream);
+                string dbData;
+                data = data.Substring(0, data.Length - 1);
+
+                try
+                {
+                    dbData = streamReader.ReadToEnd();
+
+                    foreach (var line in dbData.Split('|'))
+                    {
+                        if (line == data)
+                        {
+                            return true;
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                finally
+                {
+                    streamReader.Close();
+                    cryptoStream.Close();
+                    fileStream.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return false;
+        }
+
+        public bool ReadCryptoData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RegisterAccount(string data)
+        {
+            FileStream fileStream = null;
+            CryptoStream cryptoStream = null;
+            StreamReader streamReader = null;
+            string dbData;
+            try
+            {
+                fileStream = new FileStream("NotPasswords.bin", FileMode.OpenOrCreate);
+                cryptoStream = new CryptoStream(fileStream, aes.CreateDecryptor(key, iv), CryptoStreamMode.Read);
+                streamReader = new StreamReader(cryptoStream);
+                dbData = streamReader.ReadToEnd();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+            finally
+            {
+                streamReader.Close();
+                cryptoStream.Close();
+                fileStream.Close();
+            }
+            dbData = dbData + data;
+
+            try
+            {
+                fileStream = new FileStream("NotPasswords.bin", FileMode.OpenOrCreate);
+                cryptoStream = new CryptoStream(fileStream, aes.CreateEncryptor(key, iv), CryptoStreamMode.Write);
+
+                byte[] binaryData = new byte[dbData.Length];
+
+                for (int i = 0; i < dbData.Length; i++)
+                {
+                    binaryData[i] = (byte)dbData[i];
+                }
+
+                cryptoStream.Write(binaryData, 0, binaryData.Length);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+            finally
+            {
+                cryptoStream.Close();
+                fileStream.Close();
+            }
+            return true;
+        }
+    }
+}
