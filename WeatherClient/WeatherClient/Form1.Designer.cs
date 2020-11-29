f307747865fabe5721bdb69f654b2f02f6772bff
patryk9922 <33067910+patryk9922@users.noreply.github.com>
2020-11-29 11:36:51 +0100

    Initial commit of Client

---
+++ WeatherClient/WeatherClient/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿
+namespace WeatherClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxIPAddress = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBoxLogin = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.textBoxPort = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxLocation = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.buttonGetWeather = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxIPAddress
+            // 
+            this.textBoxIPAddress.Location = new System.Drawing.Point(86, 12);
+            this.textBoxIPAddress.Name = "textBoxIPAddress";
+            this.textBoxIPAddress.Size = new System.Drawing.Size(154, 23);
+            this.textBoxIPAddress.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(38, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "IP";
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(116, 70);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(75, 23);
+            this.buttonConnect.TabIndex = 2;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(302, 14);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(544, 493);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBoxLogin
+            // 
+            this.textBoxLogin.Location = new System.Drawing.Point(86, 123);
+            this.textBoxLogin.Name = "textBoxLogin";
+            this.textBoxLogin.Size = new System.Drawing.Size(154, 23);
+            this.textBoxLogin.TabIndex = 4;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(86, 152);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(154, 23);
+            this.textBoxPassword.TabIndex = 5;
+            this.textBoxPassword.UseSystemPasswordChar = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(12, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 21);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Login";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(3, 150);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 21);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Password";
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Enabled = false;
+            this.buttonLogin.Location = new System.Drawing.Point(116, 181);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(75, 23);
+            this.buttonLogin.TabIndex = 8;
+            this.buttonLogin.Text = "Login";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
+            // 
+            // textBoxPort
+            // 
+            this.textBoxPort.Location = new System.Drawing.Point(86, 41);
+            this.textBoxPort.Name = "textBoxPort";
+            this.textBoxPort.Size = new System.Drawing.Size(154, 23);
+            this.textBoxPort.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(38, 43);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 21);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Port";
+            // 
+            // textBoxLocation
+            // 
+            this.textBoxLocation.Location = new System.Drawing.Point(86, 230);
+            this.textBoxLocation.Name = "textBoxLocation";
+            this.textBoxLocation.Size = new System.Drawing.Size(154, 23);
+            this.textBoxLocation.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(3, 232);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 21);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Location";
+            // 
+            // buttonGetWeather
+            // 
+            this.buttonGetWeather.Enabled = false;
+            this.buttonGetWeather.Location = new System.Drawing.Point(100, 259);
+            this.buttonGetWeather.Name = "buttonGetWeather";
+            this.buttonGetWeather.Size = new System.Drawing.Size(109, 23);
+            this.buttonGetWeather.TabIndex = 13;
+            this.buttonGetWeather.Text = "Get Weather";
+            this.buttonGetWeather.UseVisualStyleBackColor = true;
+            this.buttonGetWeather.Click += new System.EventHandler(this.buttonGetWeather_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(858, 540);
+            this.Controls.Add(this.buttonGetWeather);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxLocation);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxPort);
+            this.Controls.Add(this.buttonLogin);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxLogin);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxIPAddress);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxIPAddress;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBoxLogin;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.TextBox textBoxPort;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxLocation;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button buttonGetWeather;
+    }
+}
+
---
+++ WeatherClient/WeatherClient/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Sockets;
+
+namespace WeatherClient
+{
+    public partial class Form1 : Form
+    {
+        private string ipAddress;
+        private int port;
+        TcpClient klient;
+        byte[] buffer;
+        NetworkStream stream;
+        bool connected = false;
+        public Form1()
+        {
+            InitializeComponent();
+            buffer = new byte[85];
+            textBoxIPAddress.Text = "127.0.0.1";
+            textBoxPort.Text = "2048";
+            textBoxLogin.Text = "test";
+            textBoxPassword.Text = "123";
+            textBoxLocation.Text = "Warsaw";
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            if (connected)
+            {
+                klient.Close();
+                buttonConnect.Text = "Connect";
+                connected = false;
+                buttonLogin.Enabled = false;
+                buttonGetWeather.Enabled = false;
+                buffer = new byte[85];
+                textBox1.Text = "";
+                return;
+            }
+            ipAddress = textBoxIPAddress.Text;
+            port = int.Parse(textBoxPort.Text);
+
+            if (port < 1024 || port > 65535)
+            {
+                MessageBox.Show("Wrong port number");
+                return;
+            }
+
+            klient = new TcpClient(ipAddress, port);
+
+            stream = klient.GetStream();
+
+            stream.Read(buffer, 0, 85);
+
+            if (Encoding.ASCII.GetString(buffer).Replace("\0", "") == "Login: ")
+            {
+                textBox1.Text = "Log in";
+                buttonLogin.Enabled = true;
+                buttonConnect.Text = "Disconnect";
+                connected = true;
+            }
+
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            buffer = Encoding.ASCII.GetBytes(textBoxLogin.Text);
+
+            stream.Write(buffer, 0, buffer.Length);
+            stream.Write(buffer, 0, 2);
+
+            buffer = new byte[85];
+
+            stream.Read(buffer, 0, 85);
+
+            string data = Encoding.ASCII.GetString(buffer).Replace("\0", "");
+
+            if (Encoding.ASCII.GetString(buffer).Replace("\0", "") == "Password: ")
+            {
+                buffer = Encoding.ASCII.GetBytes(textBoxPassword.Text);
+
+                stream.Write(buffer, 0, buffer.Length);
+                stream.Write(buffer, 0, 2);
+
+                buffer = new byte[85];
+
+                stream.Read(buffer, 0, buffer.Length);
+
+                textBox1.Text = Encoding.ASCII.GetString(buffer);
+
+                stream.Read(buffer, 0, buffer.Length);
+
+                textBox1.Text += Encoding.ASCII.GetString(buffer);
+
+                buttonGetWeather.Enabled = true;
+                buttonLogin.Enabled = false;
+            }
+        }
+
+        private void buttonGetWeather_Click(object sender, EventArgs e)
+        {
+            buffer = Encoding.ASCII.GetBytes(textBoxLocation.Text);
+
+            stream.Write(buffer, 0, buffer.Length);
+
+            buffer = new byte[1024];
+
+            stream.Read(buffer, 0, buffer.Length);
+            stream.Read(buffer, 0, buffer.Length);
+
+            textBox1.Text = Encoding.ASCII.GetString(buffer).Replace("\0", "");
+            stream.Read(buffer, 0, buffer.Length);
+        }
+
+    }
+}
