335e52aceb669ae22aa10cc4c7bc5ad3ad4af60d
KarolZarebski <karol.zarebski@digimuth.com>
2020-12-28 16:15:39 +0100

    Added favourite location

--- ServerGUI/Storage/Models/UserData.cs
+++ ServerGUI/Storage/Models/UserData.cs
@@ -1,11 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace Storage.Models
+﻿namespace Storage.Models
 {
     public class UserData
     {
         public long Id { get; set; }
         public string Login { get; set; }
         public byte[] Password { get; set; }
+        public string? FavouriteLocation { get; set; }
     }
 }
