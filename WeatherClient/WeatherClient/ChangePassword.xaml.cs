a8a42f9b861a784c61847f3c5080cf7d76968a4e
patryk9922 <33067910+patryk9922@users.noreply.github.com>
2020-12-29 13:31:48 +0100

    Changed Client to WPF

---
+++ WeatherClient/WeatherClient/ChangePassword.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WeatherClient
+{
+    /// <summary>
+    /// Logika interakcji dla klasy ChangePassword.xaml
+    /// </summary>
+    public partial class ChangePassword : Window
+    {
+        public ChangePassword()
+        {
+            InitializeComponent();
+        }
+
+        private void ChangePasswordButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+            this.Close();
+        }
+    }
+}
